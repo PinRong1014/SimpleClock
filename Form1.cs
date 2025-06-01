45b69fc0e9173c8e321e5afd1efb77115d27ee65
PinRong1014 <113153001@gas.fgu.edu.tw>
2025-06-01 21:41:14 +0800

    整理程式碼

--- Form1.cs
+++ Form1.cs
@@ -38,31 +38,9 @@ namespace SimpleClock
         bool isCountDownReset = true;                           // 用來紀錄是不是重新設定
         TimeSpan ts;                                            // 宣告一個時間間隔變數
 
-        // 下拉選單初始化
-        private void comboboxInitialzation()
-        {
-            // 設定小時下拉選單的選單內容，建立小時的清單，數字範圍為00-23
-            for (int i = 0; i <= 23; i++)
-            {
-                cmbHour.Items.Add(string.Format("{0:00}", i));
-                cmbCountHour.Items.Add(string.Format("{0:00}", i));
-            }
+        #region -- Tick事件 --
 
-            // 設定分鐘下拉選單的選單內容，建立分鐘的清單，數字範圍為00-59
-            for (int i = 0; i <= 59; i++)
-            {
-                cmbMin.Items.Add(string.Format("{0:00}", i));
-                cmbCountMin.Items.Add(string.Format("{0:00}", i));
-                cmbCountSecond.Items.Add(string.Format("{0:00}", i));
-            }
-
-            cmbHour.SelectedIndex = 0;
-            cmbMin.SelectedIndex = 0;
-            cmbCountHour.SelectedIndex = 0;
-            cmbCountMin.SelectedIndex = 0;
-            cmbCountSecond.SelectedIndex = 0;
-        }
-
+        // 時鐘timer1_Tick事件：每一秒執行一次
         private void timerClcok_Tick(object sender, EventArgs e)
         {
             txtTime.Text = DateTime.Now.ToString("HH:mm:ss");    // 顯示時間
@@ -104,6 +82,59 @@ namespace SimpleClock
             }
         }
 
+        // timerStopWatch_tick：每毫秒執行一次，所以更新的速度會比較快
+        private void timerStopWatch_Tick(object sender, EventArgs e)
+        {
+            txtStopWatch.Text = sw.Elapsed.ToString("hh':'mm':'ss':'fff");    // 顯示碼表時間
+        }
+
+        // 鬧鐘計時器timerAlert_tick事件：每一秒執行一次
+        private void timerAlert_tick(object sender, EventArgs e)
+        {
+            // 判斷現在時間是不是已經是鬧鐘設定時間？如果時間到了，就要播放鬧鐘聲音
+            if (strSelectTime == DateTime.Now.ToString("HH:mm"))
+                playBeep(timerAlert);
+        }
+
+        // 倒數計時器timerCountDown_Tick事件：每一秒執行一次
+        private void timerCountDown_Tick(object sender, EventArgs e)
+        {
+            txtCountDown.Text = ts.ToString("hh':'mm':'ss");    // 顯示時間
+            ts = ts.Subtract(TimeSpan.FromSeconds(1));          // 每一秒鐘將顯示時間減掉一秒
+
+            if (txtCountDown.Text == "00:00:00")
+                playBeep(timerCountDown);
+        }
+
+        #endregion
+
+        #region -- 自訂函式 --
+
+        // 下拉選單初始化
+        private void comboboxInitialzation()
+        {
+            // 設定小時下拉選單的選單內容，建立小時的清單，數字範圍為00-23
+            for (int i = 0; i <= 23; i++)
+            {
+                cmbHour.Items.Add(string.Format("{0:00}", i));
+                cmbCountHour.Items.Add(string.Format("{0:00}", i));
+            }
+
+            // 設定分鐘下拉選單的選單內容，建立分鐘的清單，數字範圍為00-59
+            for (int i = 0; i <= 59; i++)
+            {
+                cmbMin.Items.Add(string.Format("{0:00}", i));
+                cmbCountMin.Items.Add(string.Format("{0:00}", i));
+                cmbCountSecond.Items.Add(string.Format("{0:00}", i));
+            }
+
+            cmbHour.SelectedIndex = 0;
+            cmbMin.SelectedIndex = 0;
+            cmbCountHour.SelectedIndex = 0;
+            cmbCountMin.SelectedIndex = 0;
+            cmbCountSecond.SelectedIndex = 0;
+        }
+
         // 停止之前的播放
         private void stopWaveOut()
         {
@@ -114,6 +145,8 @@ namespace SimpleClock
                 waveOut = null;
             }
         }
+
+        #endregion
 
         private void btnSetAlert_Click(object sender, EventArgs e)
         {
@@ -129,12 +162,6 @@ namespace SimpleClock
             timerAlert.Stop(); // 停止鬧鐘計時器
             btnSetAlert.Enabled = true;
             btnCancelAlert.Enabled = false;
-        }
-
-        // timerStopWatch_tick：每毫秒執行一次，所以更新的速度會比較快
-        private void timerStopWatch_Tick(object sender, EventArgs e)
-        {
-            txtStopWatch.Text = sw.Elapsed.ToString("hh':'mm':'ss':'fff");    // 顯示碼表時間
         }
 
         // 啟動碼表
@@ -198,7 +225,7 @@ namespace SimpleClock
             }
         }
 
-        // timerCountDown_tick：每一秒執行一次
+        // 開始倒數計時器按鍵
         private void btnCountStart_Click(object sender, EventArgs e)
         {
             // 進行判斷，判斷是不是有按過停止計時器按鍵
@@ -229,24 +256,6 @@ namespace SimpleClock
             cmbCountHour.SelectedIndex = 0;
             cmbCountMin.SelectedIndex = 0;
             cmbCountSecond.SelectedIndex = 0;
-        }
-
-        // 鬧鐘計時器timerAlert_tick事件：每一秒執行一次
-        private void timerAlert_tick(object sender, EventArgs e)
-        {
-            // 判斷現在時間是不是已經是鬧鐘設定時間？如果時間到了，就要播放鬧鐘聲音
-            if (strSelectTime == DateTime.Now.ToString("HH:mm"))
-                playBeep(timerAlert);
-        }
-
-        // 倒數計時器timerCountDown_Tick事件：每一秒執行一次
-        private void timerCountDown_Tick(object sender, EventArgs e)
-        {
-            txtCountDown.Text = ts.ToString("hh':'mm':'ss");    // 顯示時間
-            ts = ts.Subtract(TimeSpan.FromSeconds(1));          // 每一秒鐘將顯示時間減掉一秒
-
-            if (txtCountDown.Text == "00:00:00")
-                playBeep(timerCountDown);
         }
 
         // 播放鬧鐘聲音檔函式
